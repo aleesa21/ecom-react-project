9de82e5d9bd9d7b5246f81b117f865c4a16f99f6
aleesa21 <kaflealeesa@gmail.com>
2026-06-12 21:23:14 +0545

    added checkout layout

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,5 +1,17 @@
-export default function Checkout(){
-    return(
-        <div>Checkout page</div>
-    )
+import "../css/checkout.css";
+export default function Checkout() {
+  return (
+    <section className="checkout ">
+      <div className="padding checkout_page">
+        <div className="top-checkout">
+          <div>← Back to Cart</div>
+          <h1>Checkout</h1>
+        </div>
+        <div className="down-checkout">
+            <div className="checkout_details"></div>
+            
+        </div>
+      </div>
+    </section>
+  );
 }
